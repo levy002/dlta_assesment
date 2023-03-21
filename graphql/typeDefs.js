de340ca30084c110cb44c3f69fd94a2195e73c5e
“levy-DELL” <levy.ukwi002@gmail.com>
2023-03-21 15:24:42 +0200

    Define Queries and Mutations

---
+++ graphql/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+  type countryInfo {
+    country: String
+    year: String
+    area: Int
+    totalPopulation: Int
+  }
+
+  type Query {
+    countryInfo(ID: ID!): countryInfo!
+    getCountryInfoByYear(year: String): [countryInfo]
+    getCountryInfoBYearRange(fromYear: String, toYear: String): [countryInfo]
+    getCountryInfoByHighestNumberOfPopulation: [countryInfo]
+    getCountryInfoByLowestNumberOfPopulation: [countryInfo]
+  }
+
+  input CountryInfoInput {
+    country: String
+    year: String
+    area: Int
+    totalPopulation: Int
+  }
+
+  type Mutation {
+    newcountryInfo(countryInfoInput: CountryInfoInput): countryInfo!
+    deletecountryInfo(ID: ID!): Boolean
+  }
+`;
